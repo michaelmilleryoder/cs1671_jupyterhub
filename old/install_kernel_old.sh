07ccad0d1f30343bceae78bf23cf53acbcdbb3de
Michael Miller Yoder <michaelmilleryoder@gmail.com>
2025-01-14 14:53:41 -0500

    fingers crossed

--- old/install_kernel_old.sh
+++ old/install_kernel_old.sh
@@ -1,5 +1,4 @@
 #! /bin/bash
 
-mkdir ~/.local/share/jupyter/kernels/cs1671
+mkdir -p ~/.local/share/jupyter/kernels/cs1671
 cp kernel.json ~/.local/share/jupyter/kernels/cs1671
-echo 'Installed cs1671 kernel spec'
