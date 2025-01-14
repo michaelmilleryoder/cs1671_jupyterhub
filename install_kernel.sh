b3582b097edc8b6849ffdeeb6af353bd3e8e530c
Michael Miller Yoder <michaelmilleryoder@gmail.com>
2025-01-14 14:39:01 -0500

    kernel attempts

--- install_kernel.sh
+++ install_kernel.sh
@@ -1,5 +1,3 @@
 #! /bin/bash
 
-mkdir ~/.local/share/jupyter/kernels/cs1671
-cp kernel.json ~/.local/share/jupyter/kernels/cs1671
-echo 'Installed cs1671 kernel spec'
+jupyter kernelspec install --user cs1671/
