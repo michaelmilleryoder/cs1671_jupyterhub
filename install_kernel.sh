a2bc7ee1f6e4fe7dc835357c94ffae6ee3ef6223
Michael Miller Yoder <michaelmilleryoder@gmail.com>
2025-01-17 21:14:20 -0500

    path way

--- install_kernel.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash
-
-jupyter kernelspec install --user cs1671/
--- old/install_kernel_old.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-
-mkdir -p ~/.local/share/jupyter/kernels/cs1671
-cp kernel.json ~/.local/share/jupyter/kernels/cs1671
